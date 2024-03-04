78270e095024ad6b00b42535d82492a863c9c95a
LeoDodik <119758958+LeoDodik@users.noreply.github.com>
2024-03-04 18:53:30 +0100

    Adding Files

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
+import turtle
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
-
-def move_forward():
-    tim.forward(10)
-
-def move_backwards():
-    tim.backward(10)
-
-
-def counter_clockwise():
-    tim.left(10)
-
-def clockwise():
-    tim.right(10)
-
-
-def clear_drawing():
-    tim.clear()
-    tim.home()
-
+is_race_on = False
 
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput("Make your bet", "Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
-# Set up event listeners
-screen.onkey(key="w", fun=move_forward)
-screen.onkey(key="s", fun=move_backwards)
-screen.onkey(key="a", fun=counter_clockwise)
-screen.onkey(key="d", fun=clockwise)
-screen.onkey(key="c", fun=clear_drawing)
+for turtle_index in range(0, 6):
+    turtle = Turtle(shape="turtle")
+    turtle.penup()
+    turtle.color(colors[turtle_index])
+    turtle.goto(x=-230 - 10, y=y_positions[turtle_index])
+    all_turtles.append(turtle)
 
+if user_bet:
+    is_race_on = True
 
-screen.listen()
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if user_bet == winning_color:
+                print(f"You have guessed it right, {winning_color} has won")
+            else:
+                print(f"You have not guessed it right, {winning_color} has won")
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+
 screen.exitonclick()
